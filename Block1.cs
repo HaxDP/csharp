02db45c3c0e2c4154be75b91271b0455a67213cd
“haxDP001” <legendary15988@gmail.com>
2024-10-30 19:09:00 +0200

    added 1 and 2 block

---
+++ Block1.cs
@@ -0,0 +1,41 @@
+using System;
+class Block1_Task1 {
+  static void Main() {
+    /*
+    Console.WriteLine("Enter name:");
+    string username = Console.ReadLine();
+    Console.WriteLine("Hello {0}", username);
+    */
+    /*
+    Console.WriteLine("Enter how old r u?: ");
+    int years = Convert.ToInt32(Console.ReadLine());
+    int years_in_days = years * 365;
+    Console.WriteLine("Ur years equals: {0}", years_in_days);
+    */
+    /*
+    int f, g = 10;
+    Console.WriteLine("Enter weight: ");
+    int m = Convert.ToInt32(Console.ReadLine());
+    f = m * g;
+    Console.WriteLine("Result is: {0}", f);
+    */
+    /*
+    Console.WriteLine("Enter all sides: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    int c = Convert.ToInt32(Console.ReadLine());
+    double p = (a + b + c) / 2.0;
+    double res = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+    Console.WriteLine("Result is: {0}", res);
+    */
+    /*
+    Console.WriteLine("Enter three values: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    int b = Convert.ToInt32(Console.ReadLine());
+    int c = Convert.ToInt32(Console.ReadLine());
+    int[] numbers = { a, b, c };
+    Array.Sort(numbers);
+    Console.WriteLine("Maximum: {2}, middle: {1}, minimum: {0}", numbers[0], numbers[1], numbers[2]);
+    */
+  }
+}
